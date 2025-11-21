ead8dcbcd98df8f039588569bc0ca7964670efa2
Drogobytskyy <drogobytskyy.volodymyr@lll.kpi.ua>
2025-11-21 11:02:49 +0200

    environment_settings

--- Makefile
+++ Makefile
@@ -1,20 +1,21 @@
-
 CXX = g++
+
 CXXFLAGS = -std=c++17 \
     -I/usr/local/mysql-connector-c++-9.5.0/include \
-    -I/usr/local/mysql-connector-c++-9.5.0/include/jdbc
+    -I/usr/local/mysql-connector-c++-9.5.0/include/jdbc \
+	-I./libs
 
-LDFLAGS = -L/usr/local/mysql-connector-c++-9.5.0/lib64 \
+LDFLAGS = -L/Applications/MySQLWorkbench.app/Contents/Frameworks \
     -lmysqlcppconn
 
 TARGET = app
-SRC = main.cpp
 
+SRC = main.cpp
 
 all: $(TARGET)
 
 $(TARGET): $(SRC)
-	$(CXX) $(SRC) $(CXXFLAGS) $(LDFLAGS) -o $(TARGET)
+	$(CXX) $(SRC) $(CXXFLAGS) -L/Applications/MySQLWorkbench.app/Contents/Frameworks -lmysqlcppconn -Wl,-rpath,/Applications/MySQLWorkbench.app/Contents/Frameworks -o $(TARGET)
 
 clean:
 	rm -f $(TARGET)
--- main.cpp
+++ main.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include <memory>
+#include <jdbc/mysql_driver.h>
+#include <jdbc/mysql_connection.h>
+#include <jdbc/cppconn/statement.h>
+#include <jdbc/cppconn/resultset.h>
+#include "libs/json.hpp" 
+
+using json = nlohmann::json;
+
+struct DbConfig {
+    std::string host;
+    std::string user;
+    std::string password;
+    std::string database;
+};
+
+DbConfig loadConfig(const std::string& path) {
+    std::ifstream file(path);
+    json j;
+    file >> j;
+
+    return {
+        j["host"],
+        j["user"],
+        j["password"],
+        j["database"]
+    };
+}
+
+int main() {
+    try {
+        DbConfig cfg = loadConfig("config.json");
 
 
+        sql::mysql::MySQL_Driver *driver;
+        std::unique_ptr<sql::Connection> con;
 
-int main(void){
-    std::cout << std::endl;
+        driver = sql::mysql::get_mysql_driver_instance();
+        con.reset(driver->connect(cfg.host, cfg.user, cfg.password));
+        con->setSchema(cfg.database);
 
+        std::unique_ptr<sql::Statement> stmt(con->createStatement());
+        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT * FROM Car"));
 
-
-
-
-
-    std::cout << std::endl;
-    return 0;
+        while (res->next()) {
+            std::cout << res->getString(1) << std::endl;
+        }
+    }
+    catch (sql::SQLException &e) {
+        std::cerr << "SQLException: " << e.what() << std::endl;
+    }
 }
